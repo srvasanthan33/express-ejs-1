6ce18ad66accf527e27aef6ad62dc4ad40876bb1
Vasanthan <srvasanthan33@outlook.com>
2023-05-03 17:55:27 +0530

    Update. Service page upadated to queried on console

--- servicePage.js
+++ servicePage.js
@@ -1,16 +1,46 @@
 const express = require("express");
 const router = express();
 
-router.get("/", (request, response) => {
-    response.send("this is service page");
-});
+router
+    .route("/")
+    .get((request, response) => {
+        response.json(Services);
+    })
+    .post((request, response) => {
+        console.log(request.body.serviceName);
+
+        let isServiceValidTrue = true;
+        if (isServiceValidTrue) {
+            Services.push({ name: request.body.serviceName });
+            response.redirect;
+        }
+    });
 
 router.get("/new", (request, response) => {
     response.render("services/newService");
 });
 
-router.post("/", (request, response) => {
-    console.log(request.body.serviceName);
+const Services = [
+    {
+        name: "Service Page"
+    },
+    {
+        name: "Game Development"
+    },
+    {
+        name: "Pen Testing"
+    },
+    {
+        name: "Backend Developing"
+    }
+];
+router.get("/:id", (request, response) => {
+    response.send(request.service.name);
 });
 
+router.param("id", (request, response, next, id) => {
+    console.log("Service queried : " + Services[id].name);
+    request.service = Services[id];
+    next();
+});
 module.exports = router;
